4e3c4ef48b9e8e9a37138333140103308207dcc0
Bridoo Saro <sarobaridoo@gmail.com>
2024-03-24 01:52:20 -0700

    fixed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-file { '/tmp/school':
-	ensure => 'file',
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => 'I love Puppet'
+# create file with certain requirements
+file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
-	
