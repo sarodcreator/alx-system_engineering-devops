d4b111ee1100c4966eca0f127e766e21ea72dd91
Baridoo Saro Biragbara <sarobaridoo@gmail.com>
2024-06-19 07:26:34 -0700

    fixed

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
-"""Script that returns top 10 hot posts of a subreddit"""
+"""
+Function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Function that prints top 10 posts of a subreddit"""
-    if subreddit is None or not isinstance(subreddit, str):
-        print(None)
-        return
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, print None.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
+    )
 
-    headers = {'User-Agent': 'selBot/2.0'}
-    URL = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-
-    try:
-        response = requests.get(URL, headers=headers, allow_redirects=False)
-        response.raise_for_status()
-        data = response.json()
-        posts = data['data']['children']
-
-        if not posts:
-            print("None")
-
-        else:
-            for post in posts:
-                title = post['data']['title']
-                print(title)
-
-    except requests.exceptions.RequestException:
-        print(None)
-
-    except (KeyError, ValueError):
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            print(title)
+    else:
         print(None)
